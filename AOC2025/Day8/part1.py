6f874381262f551efb4bf67b8480204de17a3c9c
Nathan CHAMPAGNE <nathan.champagne@epita.fr>
2025-12-08 22:40:51 +0100

    add 2025 day8

---
+++ AOC2025/Day8/part1.py
@@ -0,0 +1,36 @@
+from functools import reduce
+from AOC2025.aoc_tools import *
+
+def circuit_merge(circuit, x, y):
+    ind = -1
+    for j in range(len(circuit)):
+        if y in circuit[j] and x not in circuit[j]:
+            ind = j
+            break
+    if ind >= 0:
+        for j in range(len(circuit)):
+            if x in circuit[j]:
+                circuit[j] |= circuit[ind]
+                del circuit[ind]
+                break
+
+
+@tester()
+def main(lines):
+    coords = np.array(pll(lines, d=','))
+    nb_pts = coords.shape[0]
+    distance = np.full((nb_pts, nb_pts), np.inf)
+    for i in range(nb_pts):
+        for j in range(i+1, nb_pts):
+            distance[i, j] =  np.linalg.norm(coords[i] - coords[j])
+
+    circuit = [{c} for c in range(nb_pts)]
+    for i in range(1000):
+        ind = np.argmin(distance)
+        y, x = ind % nb_pts, ind // nb_pts
+        circuit_merge(circuit, x, y)
+        distance[x, y] = np.inf
+
+    return reduce(lambda acc, x: acc * x, sorted([len(c) for c in circuit])[-3:], 1)
+
+
